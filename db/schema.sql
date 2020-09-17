2141bacb1d42149498330aaf6372a00710301916
Aaron Jamal <aaron.jamal09@gmail.com>
2020-09-17 12:35:06 -0400

    creating a export for the orm

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,9 @@ USE burger_db;
 
 CREATE TABLE burgers
 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR(30),
+    id INT
+    AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR
+    (30),
     devoured BOOLEAN,
 );
